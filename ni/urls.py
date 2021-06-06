1dcbe5b4b160e0990b9e4a60ffd2e55434d73d10
abdelrhman <majorabdo12@gmail.com>
2021-06-06 16:40:49 +0200

    jwa,s

--- ni/urls.py
+++ ni/urls.py
@@ -1,6 +1,9 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path,include
 from main.views import mainApp,map,rooms,contact,explore,spa,resta,feed,login_v,register,offers,about,mapAlex,mapGiza,mapLuxor,mapSharm,logout_view,room_view,reservation_view,deleteview
+from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', mainApp),
@@ -23,4 +26,4 @@ urlpatterns = [
     path('rooms/<int:id>/',room_view,name='room_view'),
     path('reserve/',reservation_view),
     path('reserve/<int:id>/', deleteview, name='reserve_view'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
